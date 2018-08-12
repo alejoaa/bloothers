9b9da2bae6195516f54c3cc1d4f3aafcf721a47b
Alejandro <alejo_aguirrea@outlook.com>
2018-08-12 05:04:17 -0500

    - initial commit - complete donation.ejs page

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+var express = require('express'),
+	path = require('path');
+var app = express();
+
+
+app.set("view engine", "ejs");
+
+// define paths to modules in node_modules
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
+
+// define views folder according to developing mode (developing or production)
+if (process.env.DEV_ENV == "true") {
+	app.set('views', path.join(__dirname, '/app/views'));
+	app.use(express.static(__dirname + '/app/temp'));
+	app.use(express.static(__dirname + '/app/assets'));
+} else {
+	app.set('views', path.join(__dirname, '/dist/views'));
+	app.use(express.static(__dirname + "/dist/assets"));
+}
+
+// Routes
+
+app.get('/', function(req, res) {
+    res.redirect('/donar');
+});
+
+app.get('/donar', function(req, res) {
+    res.render('donation.ejs');
+});
+
+app.listen(8000, function(){
+	console.log("Devoloping mode set to: " + process.env.DEV_ENV);
+	console.log('App is listening on port 8000!');
+});
